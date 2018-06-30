25f31c9c89c2ebb1c5cefa5db9db32de4fec52eb
hungnguyen0606 <nguyenphanmanhhung@gmail.com>
2018-06-30 14:54:36 +0700

    rec

---
+++ recognition.py
@@ -0,0 +1,22 @@
+import boto3
+
+BUCKET = "amazon-rekognition"
+KEY = "dog.jpeg"
+
+def detect_labels(bucket, key, max_labels=10, min_confidence=90, region="us-east-1"):
+	rekognition = boto3.client("rekognition", region)
+	response = rekognition.detect_labels(
+		Image={
+			"S3Object": {
+				"Bucket": bucket,
+				"Name": key,
+			}
+		},
+		MaxLabels=max_labels,
+		MinConfidence=min_confidence,
+	)
+	return response['Labels']
+
+
+for label in detect_labels(BUCKET, KEY):
+	print ("{Name} - {Confidence}%".format(**label))
