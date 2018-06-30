665e58ae0047814774c30110b9cb56359f0a7d65
Vo Anh <Bi@Vos-MacBook-Pro-2.local>
2018-06-30 17:56:57 +0700

    ok

--- rekog.py
+++ rekog.py
@@ -1,9 +1,9 @@
 import boto3
-
+import os
 BUCKET = "amazon-rekognition-cin"
 KEY = "1.jpg"
 
-def detect_labels(bucket, key, max_labels=10, min_confidence=90, region="us-east-1"):
+def detect_labels(bucket, key, max_labels=10, min_confidence=.6, region="us-east-1"):
     rekognition = boto3.client("rekognition", region)
     response = rekognition.detect_labels(
         Image={
@@ -17,16 +17,20 @@ def detect_labels(bucket, key, max_labels=10, min_confidence=90, region="us-east
     )
     return response['Labels']
 def syn():
-    syn_image = 'aws s3 sync image s3:/{}'.format(BUCKET)
+    syn_image = 'aws s3 sync image s3://{}'.format(BUCKET)
     os.system(syn_image)
 
 from pyson_utils import image
 paths = image.get_paths("image", "jpg")
 
 def path2objects(path):
-    KEY = path.split('/')[-1]
+    KEY = path#path.split('/')[-1]
     rv = detect_labels(BUCKET, KEY)
     # for label in rv:
     #     print ("{Name} - {Confidence}%".format(**label))
     return rv
 # aws rekognition detect-labels --image "S3Object={Bucket="clicksmile",Name="/Users/Bi/Downloads/20180614_180755.jpg"}"
+syn()
+json_1 = path2objects('1/1.jpg')
+json_2 = path2objects('2/2.jpg')
+print(json_1, json_2)
